0a262a0da907bfc0b6fdf15f4c483cd4044d6432
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-04-02 18:57:11 +0430

    add Dtos and Static Roles

---
+++ JwtAuthAspNet7WebAPI/Core/Dtos/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthAspNet7WebAPI.Core.Dtos
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; } 
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
---
+++ JwtAuthAspNet7WebAPI/Core/Dtos/UpdatePermissionDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthAspNet7WebAPI.Core.Dtos
+{
+    public class UpdatePermissionDto
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; } 
+      
+    }
+}
