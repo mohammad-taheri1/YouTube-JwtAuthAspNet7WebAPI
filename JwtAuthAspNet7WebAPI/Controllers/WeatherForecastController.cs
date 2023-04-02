3290b480d1288e391d79ff58e225f063dcba3602
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-04-02 18:31:47 +0430

    refactor Wether resources

--- JwtAuthAspNet7WebAPI/Controllers/WeatherForecastController.cs
+++ JwtAuthAspNet7WebAPI/Controllers/WeatherForecastController.cs
@@ -9,25 +9,14 @@ namespace JwtAuthAspNet7WebAPI.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
         {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(Summaries);
         }
     }
 }
